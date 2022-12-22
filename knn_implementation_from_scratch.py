92f2859093b3573ef809f881fae5b5e976bd2166
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-12-22 21:54:22 +0500

    get_majority_vote implemented

--- knn_implementation_from_scratch.py
+++ knn_implementation_from_scratch.py
@@ -10,6 +10,9 @@
     In case of classification: Take the majority vote and assign that class to the data point.
 '''
 
+import numpy as np
+
+
 def euclidean_dist(x1, x2):
     return np.sqrt(np.sum(x1-x2)**2)
 
@@ -18,17 +21,39 @@ class KNN:
     def __init__(self, k=3):
         self.k = k
 
-
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
 
-
     def predict(self, X):
-        predictions = [get_majority_vote(x) for x in X]
+        predictions = [get_class(x) for x in X]
         return predictions
 
-
-    def get_majority_vote(self, x):
+    def get_class(self, x):
         # Compute the distances between all other data points in X_train
-        return x
+        distances = [euclidean_dist(x, x_train) for x_train in self.X_train]
+
+        # Get the closest K neighbors
+        k_indices = np.argsort(distances)[:self.k]
+        k_labels = [y_train[i] for i in k_indices]
+
+        # Get the majority vote
+
+    def get_majority_vote(k_labels):
+        label_counts = {}
+
+        for label in k_labels:
+            if label not in label_counts:
+                label_counts[label] = 1
+            else:
+                label_counts[label] = label_counts[label] + 1
+
+        max_label = 0
+        max_occurence = 0
+
+        for key, value in k_labels.items():
+            if max_occurence < label_counts[key]:
+                max_label = key
+                max_occurence = label_counts[key]
+
+        return max_label
