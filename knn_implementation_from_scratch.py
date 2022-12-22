221c0d52a26ab1128efdf10d2c7b29800a8cf0ad
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-12-22 20:11:32 +0500

    knn_implementation_init

---
+++ knn_implementation_from_scratch.py
@@ -0,0 +1,11 @@
+'''
+    HOW DOES KNN WORK?
+
+    Given a data point,
+    1. Calculate the distance between the data point and all other data points in the dataset,
+    2. Choose the K closest points,
+
+    3. THEN,
+    In case of regression: Take the average of the closest K values and assign it to the data point.
+    In case of classification: Take the majority vote and assign that class to the data point.
+'''
