b8a3a8a110ef0484d28f34667ace60d40cb062e9
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-12-22 21:42:07 +0500

    euclidean_dist implemented

--- knn_implementation_from_scratch.py
+++ knn_implementation_from_scratch.py
@@ -10,23 +10,25 @@
     In case of classification: Take the majority vote and assign that class to the data point.
 '''
 
+def euclidean_dist(x1, x2):
+    return np.sqrt(np.sum(x1-x2)**2)
+
 
 class KNN:
     def __init__(self, k=3):
         self.k = k
 
+
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
+
 
     def predict(self, X):
         predictions = [get_majority_vote(x) for x in X]
         return predictions
 
 
-`
-
-
-def get_majority_vote(self, x):
-    # Compute the distances between all other data points in X_train
-    return x
+    def get_majority_vote(self, x):
+        # Compute the distances between all other data points in X_train
+        return x
