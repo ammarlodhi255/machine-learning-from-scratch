f9d521c4c3c7703871932bda028b0c4cdc1a830a
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-12-22 21:38:45 +0500

    minor changes

--- knn_implementation_from_scratch.py
+++ knn_implementation_from_scratch.py
@@ -9,3 +9,24 @@
     In case of regression: Take the average of the closest K values and assign it to the data point.
     In case of classification: Take the majority vote and assign that class to the data point.
 '''
+
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        predictions = [get_majority_vote(x) for x in X]
+        return predictions
+
+
+`
+
+
+def get_majority_vote(self, x):
+    # Compute the distances between all other data points in X_train
+    return x
